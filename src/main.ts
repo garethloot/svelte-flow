09322075751c2cf1d77238c1554d37f2bf0b29d0
Gareth Loot <gareth@fishonfire.com>
2023-02-24 14:06:16 +0100

    umd

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import Products from './lib/Products.svelte'
 import Counter from './lib/Counter.svelte'
 
-export { Products, Counter }
+const Components = {
+  Products,
+  Counter,
+}
+
+const createComponent = (component: any, target: string) => {
+  new component({
+    target: document.getElementById(target),
+  })
+}
+
+export { Components, createComponent }
