45955e60a0d14c2f5a251309a0f784632a9a74b0
Gareth Loot <gareth@fishonfire.com>
2023-02-24 14:40:14 +0100

    js rename

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: ['./src/main.ts'],
-      formats: ['umd'],
+      formats: ['umd', 'es'],
       name: 'SvelteFlow',
       fileName: 'svelte-flow',
     },
